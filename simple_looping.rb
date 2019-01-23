6d4c785fddfff647a30d8cd1ee3a6c61486c0c46
Ammar Ali <funnyguy118@gmail.com>
2019-01-23 21:23:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,7 +24,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = number_of_times
   
-  while counter < 0 do
+  while counter > 0 do
     puts phrase
     counter -= 1
   end
